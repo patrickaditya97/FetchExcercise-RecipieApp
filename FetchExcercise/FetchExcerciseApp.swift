7ddea618b35109bec5047dc2fdf4e0f012d85ff2
Aditya PM <mpatrick97@gmail.com>
2024-07-11 12:41:23 -0500

    Initial Commit

---
+++ FetchExcercise/FetchExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchExcerciseApp.swift
+//  FetchExcercise
+//
+//  Created by Aditya on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
